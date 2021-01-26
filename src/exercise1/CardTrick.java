44aa98024a8b8367ade3b9e6700c2fed527830df
Mshah-786 <shahsyem@sheridancollege.ca>
2021-01-26 16:44:42 -0500

    Exercise 1 completed

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,9 +1,11 @@
 package exercise1;
 
+import java.util.Scanner;
+
 /**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
- * To be used as starting code in Exercise
+ * A class that fills a hand of 7 cards with random Card Objects and then asks
+ * the user to pick a card. It then searches the array of cards for the match to
+ * the user's card. To be used as starting code in Exercise
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2021
@@ -11,37 +13,54 @@ package exercise1;
 public class CardTrick {
 
     public static void main(String[] args) {
-        
+
         Card[] hand = new Card[7];
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int randSuit = (int) (Math.random() * 3);
+            int randValue = (int) (Math.random() * 13 + 1);
+            card.setValue(randValue);
+            card.setSuit(Card.SUITS[randSuit]);
+            hand[i] = card;
         }
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here.
         // If the guess is successful, invoke the printInfo() method below
-        
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < hand.length; i++) {
+            System.out.println("Enter guess suit: (0-3) ");
+            int guessSuit = s.nextInt();
+            System.out.println("Enter guess value: ");
+            int guessValue = s.nextInt();
+            
+            for (int j = 0; j < hand.length; j++) {
+                if (Card.SUITS[guessSuit].equals(hand[j].getSuit()) && guessValue == hand[j].getValue()) {
+                    printInfo();
+                }
+
+            }
+        }
+
     }
 
     /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
+     * A simple method to print out personal information. Follow the
+     * instructions to replace this information with your own.
      */
     private static void printInfo() {
-    
+
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
-        
+
         System.out.println("My name is Paul, but you can call me prof, Paul or sir");
         System.out.println();
-        
+
         System.out.println("My career ambitions:");
         System.out.println("-- Start writing articles/posts on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
+        System.out.println();
 
         System.out.println("My hobbies:");
         System.out.println("-- Playing with technology");
@@ -49,11 +68,11 @@ public class CardTrick {
         System.out.println("-- Cooking");
         System.out.println("-- Listening to music");
         System.out.println();
-        
+
         System.out.println("Music I like:");
         System.out.println("-- Pink Floyd / David Gilmore");
         System.out.println("-- Green Day");
-        System.out.println("-- Many other styles and genres as well...");    
+        System.out.println("-- Many other styles and genres as well...");
     }
 
 }
