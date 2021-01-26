d6fa9a3c12dbaab546551c56cceceb57dc0b0d6e
Andra <Andra@10.0.0.195>
2021-01-26 16:04:17 -0500

    Finished Code

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,25 +1,58 @@
 package exercise1;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 2021
+ * @author AASFLC
+ * @author Andraws Putros Jan 2021
  */
-public class CardTrick {
+public class CardTrick
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         
         Card[] hand = new Card[7];
+        Random rand = new Random();
+         int low = 1;
+         int high = 14;
 
-        for (int i = 0; i < hand.length; i++) {
+        for (int i = 0; i < hand.length; i++)
+        {
+            int value = rand.nextInt(high-low) + low;
+            int suit = rand.nextInt(4);
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+           
+            card.setValue(value);
+            card.setSuit(Card.SUITS[suit]);
+            
+            hand[i]=card;
+            //System.out.println(hand[i].getValue() + hand[i].getSuit());
         }
 
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        
+        System.out.println("Enter your card value (1-13):");
+        int cardValue = scanner.nextInt();  // Read user input
+        System.out.println("Card value is: " + cardValue);  // Output user input
+        
+        String empty = scanner.nextLine(); //Empty scanner to read next line
+        
+        System.out.println("Enter your card suit (Clubs, Spades, Diamonds, Hearts):");
+        String cardSuit = scanner.nextLine();  // Read user input
+        System.out.println("Card suit is: " + cardSuit);  // Output user input
+        
+        for (int i = 0; i < hand.length; i++)
+        {
+            if(cardValue == hand[i].getValue() && cardSuit.equals(hand[i].getSuit()))
+            {
+                printInfo();
+            }
+        }
+        
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here.
         // If the guess is successful, invoke the printInfo() method below
